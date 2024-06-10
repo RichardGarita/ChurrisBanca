080b07650a52ff2bb99d1152a7077e4643c22e1e
Dylan Tenorio Rojas <dylantr2001@gmail.com>
2024-06-10 14:05:50 -0600

    Sessions

--- back-end/app/index.js
+++ back-end/app/index.js
@@ -1,13 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors'); // Importa el middleware cors
+const cors = require("cors"); // Importa el middleware cors
 
-const api = require('./routes/api');
+const api = require("./routes/api");
 
 // Middleware para el manejo de solicitudes JSON
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use('/api', api);
+app.use("/api", api);
 
 module.exports = app;
